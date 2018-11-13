68cd771d416860a9a5a18ee04210bc0e69152095
Nevyn Bengtsson <nevyn.jpg@gmail.com>
2018-11-13 23:09:52 +0100

    Class wrapper around unsafe AlloClient

--- Assets/NetworkController.cs
+++ Assets/NetworkController.cs
@@ -6,14 +6,50 @@ using System.Threading;
 using System.Runtime.InteropServices;
 using UnityEngine;
 
+class AlloClient
+{
+    private unsafe AlloClientInt *client;
+    public AlloClient()
+    {
+        unsafe
+        {
+            client = AlloClientInt.allo_connect();
+        }
+    }
+    public void SetIntent(AlloIntent intent)
+    {
+        unsafe
+        {
+            AlloClientInt.SetIntentFun setIntent = (AlloClientInt.SetIntentFun)Marshal.GetDelegateForFunctionPointer(client->set_intent, typeof(AlloClientInt.SetIntentFun));
+            setIntent(client, intent);
+        }
+    }
+    public void Poll()
+    {
+        unsafe
+        {
+            AlloClientInt.PollFun poll = (AlloClientInt.PollFun)Marshal.GetDelegateForFunctionPointer(client->poll, typeof(AlloClientInt.PollFun));
+            poll(client);
+        }
+    }
+    public void Disconnect(int reason)
+    {
+        unsafe
+        {
+            AlloClientInt.DisconnectFun disconnect = (AlloClientInt.DisconnectFun)Marshal.GetDelegateForFunctionPointer(client->disconnect, typeof(AlloClientInt.DisconnectFun));
+            disconnect(client, reason);
+        }
+    }
+}
+
 [StructLayout(LayoutKind.Sequential)]
-struct AlloClient
+struct AlloClientInt
 {
     [DllImport("liballonet")]
     public unsafe static extern bool allo_initialize(bool redirect_stdout);
 
     [DllImport("liballonet")]
-    public unsafe static extern AlloClient *allo_connect();
+    public unsafe static extern AlloClientInt* allo_connect();
 
     public IntPtr set_intent;
     public IntPtr interact;
@@ -29,21 +65,21 @@ struct AlloClient
     public IntPtr _internal;
 
     [UnmanagedFunctionPointer(CallingConvention.StdCall)]
-    public unsafe delegate void SetIntentFun(AlloClient *client, AlloIntent intent);
+    public unsafe delegate void SetIntentFun(AlloClientInt* client, AlloIntent intent);
 
     [UnmanagedFunctionPointer(CallingConvention.StdCall)]
-    public unsafe delegate void InteractFun(AlloClient *client, string entityId, string cmd);
+    public unsafe delegate void InteractFun(AlloClientInt* client, string entityId, string cmd);
 
     [UnmanagedFunctionPointer(CallingConvention.StdCall)]
-    public unsafe delegate void DisconnectFun(AlloClient *client);
+    public unsafe delegate void DisconnectFun(AlloClientInt* client, int reason);
 
     [UnmanagedFunctionPointer(CallingConvention.StdCall)]
-    public unsafe delegate void PollFun(AlloClient *client);
+    public unsafe delegate void PollFun(AlloClientInt* client);
 
     [UnmanagedFunctionPointer(CallingConvention.StdCall)]
-    public unsafe delegate void StateCallbackFun(AlloClient *client, ref AlloState state);
+    public unsafe delegate void StateCallbackFun(AlloClientInt* client, ref AlloState state);
     [UnmanagedFunctionPointer(CallingConvention.StdCall)]
-    public unsafe delegate void InteractionCallbackFun(AlloClient *client, string senderEntityId, string receiverEntityId, string cmd);
+    public unsafe delegate void InteractionCallbackFun(AlloClientInt* client, string senderEntityId, string receiverEntityId, string cmd);
 }
 
 [StructLayout(LayoutKind.Sequential)]
@@ -81,44 +117,35 @@ struct AlloEntity
 
 public class NetworkController : MonoBehaviour
 {
-    unsafe AlloClient *client;
+    AlloClient client;
+    int frameCount;
 
 
     void Start()
     {
-        if(!AlloClient.allo_initialize(true)) {
+        if(!AlloClientInt.allo_initialize(true)) {
             throw new Exception("Unable to initialize AlloNet");
         }
-
-        AlloIntent intent = new AlloIntent
-        {
-            zmovement = 1
-        };
-
-        unsafe
-        {
-            client = AlloClient.allo_connect();
-            AlloClient.SetIntentFun setIntent = (AlloClient.SetIntentFun)Marshal.GetDelegateForFunctionPointer(client->set_intent, typeof(AlloClient.SetIntentFun));
-            setIntent(client, intent);
-        }
+        client = new AlloClient();
     }
 
     void Update()
     {
-        unsafe
+        if (frameCount++ % 3 == 0) // only send @ 20hz
         {
-            AlloClient.PollFun poll = (AlloClient.PollFun)Marshal.GetDelegateForFunctionPointer(client->poll, typeof(AlloClient.PollFun));
-            poll(client);
+            AlloIntent intent = new AlloIntent();
+            intent.zmovement = Input.GetKey(KeyCode.W) ? 1 : Input.GetKey(KeyCode.S) ? -1 : 0;
+            intent.xmovement = Input.GetKey(KeyCode.A) ? 1 : Input.GetKey(KeyCode.D) ? -1 : 0;
+            // actually, SetIntent shouldn't send it; Poll should
+            client.SetIntent(intent);
         }
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
 
-        }
+        client.Poll();
     }
 
     void OnApplicationQuit()
     {
-
+        client.Disconnect(0);
     }
 
 
