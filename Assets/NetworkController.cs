b7720923b866876748719378baec76ee7a79b531
Nevyn Bengtsson <nevyn.jpg@gmail.com>
2018-11-13 22:00:06 +0100

    Start wrapping allonet

--- Assets/NetworkController.cs
+++ Assets/NetworkController.cs
@@ -1,91 +1,111 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Net.Sockets;
 using System.Text;
 using System.Threading;
+using System.Runtime.InteropServices;
 using UnityEngine;
 
+[StructLayout(LayoutKind.Sequential)]
+struct AlloClient
+{
+    [DllImport("liballonet")]
+    public unsafe static extern AlloClient *allo_connect();
 
-public class NetworkController : MonoBehaviour {
-    private TcpClient socket;
-    private Thread consumerThread;
+    public IntPtr set_intent;
+    public IntPtr interact;
+    public IntPtr disconnect;
+
+    public IntPtr poll;
+
+    public IntPtr state_callback;
+    public IntPtr interaction_callback;
+
+    // internal
+    public AlloState state;
+    public IntPtr _internal;
+
+    [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+    public unsafe delegate void SetIntentFun(AlloClient *client, AlloIntent intent);
+
+    [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+    public unsafe delegate void InteractFun(AlloClient *client, string entityId, string cmd);
+
+    [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+    public unsafe delegate void DisconnectFun(AlloClient *client);
+
+    [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+    public unsafe delegate void PollFun(AlloClient *client);
+
+    [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+    public unsafe delegate void StateCallbackFun(AlloClient *client, ref AlloState state);
+    [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+    public unsafe delegate void InteractionCallbackFun(AlloClient *client, string senderEntityId, string receiverEntityId, string cmd);
+}
+
+[StructLayout(LayoutKind.Sequential)]
+struct AlloState
+{
+    public Int64 revision;
+    public unsafe AlloEntity* entityHead;
+};
+
+[StructLayout(LayoutKind.Sequential)]
+struct AlloIntent
+{
+    public double zmovement;
+    public double xmovement;
+    public double yaw;
+    public double pitch;
+};
+
+[StructLayout(LayoutKind.Sequential)]
+struct AlloVector
+{
+    public double x, y, z;
+};
+
+[StructLayout(LayoutKind.Sequential)]
+struct AlloEntity
+{
+    public IntPtr id; // to string
+    public AlloVector position;
+    public AlloVector rotation;
+    public unsafe AlloEntity* le_next;
+}
+
+
+
+public class NetworkController : MonoBehaviour
+{
+    unsafe AlloClient *client;
+
 
     void Start()
     {
-        ConnectToTcpServer();
+        AlloIntent intent = new AlloIntent();
+        intent.zmovement = 1;
+
+        unsafe
+        {
+            client = AlloClient.allo_connect();
+            AlloClient.SetIntentFun setIntent = (AlloClient.SetIntentFun)Marshal.GetDelegateForFunctionPointer(client->set_intent, typeof(AlloClient.SetIntentFun));
+            setIntent(client, intent);
+        }
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Debug.Log("yo2: ");
-            SendMessage();
+
         }
     }
 
     void OnApplicationQuit()
     {
-        socket.Close();
+
     }
 
 
-    /// <summary>   
-    /// Setup socket connection.    
-    /// </summary>  
-    private void ConnectToTcpServer()
-    {
-        consumerThread = new Thread(new ThreadStart(ListenForData));
-        consumerThread.IsBackground = true;
-        consumerThread.Start();
-    }
-
-    /// <summary>   
-    /// Runs in background consumerThread; Listens for incomming data.     
-    /// </summary>     
-    private void ListenForData()
-    {
-        socket = new TcpClient("localhost", 16016);
-        Byte[] bytes = new Byte[1024];
-        while (true)
-        {
-            // Get a stream object for reading              
-            using (NetworkStream stream = socket.GetStream())
-            {
-                int length;
-                // Read incomming stream into byte arrary.                  
-                while ((length = stream.Read(bytes, 0, bytes.Length)) != 0)
-                {
-                    var incommingData = new byte[length];
-                    Array.Copy(bytes, 0, incommingData, 0, length);
-                    // Convert byte array to string message.                        
-                    string serverMessage = Encoding.ASCII.GetString(incommingData);
-                    Debug.Log("server message received as: " + serverMessage);
-                }
-            }
-        }
-    }
-
-    private void SendMessage()
-    {
-        if (socket == null)
-        {
-            throw new Exception("Not connected, can't send");
-        }
-
-        // Get a stream object for writing.             
-        NetworkStream stream = socket.GetStream();
-        if (!stream.CanWrite)
-        {
-            throw new Exception("No write stream");
-        }
-
-        string clientMessage = "w\n";
-        // Convert string message to byte array.                 
-        byte[] clientMessageAsByteArray = Encoding.ASCII.GetBytes(clientMessage);
-        // Write byte array to socketConnection stream.                 
-        stream.Write(clientMessageAsByteArray, 0, clientMessageAsByteArray.Length);
-        Debug.Log("Client sent her message - should be received by server");
-    }
 }
