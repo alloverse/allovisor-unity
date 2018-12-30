63cf2f3f934c2408cf348c6c762050f51833c0b1
Nevyn Bengtsson <nevyn.jpg@gmail.com>
2018-12-30 14:57:44 +0100

    Mac build script

---
+++ Assets/Code/Editor/AlloVisorBuild.cs
@@ -0,0 +1,72 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEditor.Build.Reporting;
+using UnityEditor.iOS.Xcode;
+
+public class BuildPlayerExample : MonoBehaviour
+{
+    [MenuItem("Build/Upgrade allonet to latest master")]
+    public static void UpgradeAllonet()
+    {
+        Debug.Log("Not implemented yet");
+    }
+
+    // inspo: https://gist.github.com/sanukin39/997d8364d16c5c27dae75a3bc1f1f045
+    [MenuItem("Build/Build for MacOS")]
+    public static void MacBuild()
+    {
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.scenes = new[] { "Assets/Menu/Menu.unity", "Assets/Scenes/NetworkScene.unity" };
+        buildPlayerOptions.locationPathName = "Build/Mac/Alloverse Visor";
+        buildPlayerOptions.target = BuildTarget.StandaloneOSX;
+        buildPlayerOptions.options = BuildOptions.None;
+
+        BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+        BuildSummary summary = report.summary;
+
+        // Add URL schemes
+        PlistDocument info = new PlistDocument();
+        string infoPath = "Build/Mac/Alloverse Visor.app/Contents/Info.plist";
+        info.ReadFromFile(infoPath);
+
+        var urlTypes = info.root.CreateArray("CFBundleURLTypes");
+        var placeType = urlTypes.AddDict();
+        placeType.SetString("CFBundleURLName", "alloverse-place");
+        var placeTypeSchemes = placeType.CreateArray("CFBundleURLSchemes");
+        placeTypeSchemes.AddString("alloverse-place");
+        placeTypeSchemes.AddString("alloplace");
+
+        var applianceType = urlTypes.AddDict();
+        applianceType.SetString("CFBundleURLName", "alloverse-appliance");
+        var applianceTypeSchemes = applianceType.CreateArray("CFBundleURLSchemes");
+        applianceTypeSchemes.AddString("alloverse-appliance+http");
+        applianceTypeSchemes.AddString("alloverse-appliance+https");
+        applianceTypeSchemes.AddString("alloappliance+http");
+        applianceTypeSchemes.AddString("alloappliance+https");
+        applianceTypeSchemes.AddString("alloapp+http");
+        applianceTypeSchemes.AddString("alloapp+https");
+
+        info.WriteToFile(infoPath);
+
+        if (summary.result == BuildResult.Succeeded)
+        {
+            Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+        }
+
+        if (summary.result == BuildResult.Failed)
+        {
+            Debug.Log("Build failed");
+        }
+    }
+
+    [MenuItem("Build/Build and Run")]
+    public static void BuildAndRun()
+    {
+        MacBuild();
+        var proc = new System.Diagnostics.Process();
+        proc.StartInfo.FileName = "Build/Mac/Alloverse Visor.app/Contents/MacOS/allovisor";
+        proc.Start();
+
+    }
+
+}
