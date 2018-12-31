9f93540f69e68262b9a25d49b4a5c1892ffb50b2
Nevyn Bengtsson <nevyn.jpg@gmail.com>
2018-12-31 11:12:05 +0100

    WIP store old places in settings
    lol crashes now, gotta fix that...

--- Assets/MenuScript.cs
+++ Assets/MenuScript.cs
@@ -13,6 +13,53 @@ public class MenuParameters
     public static string lastError;
 }
 
+public struct PlaceDescriptor
+{
+    public string url;
+    public string name;
+    public PlaceDescriptor(string url, string name)
+    {
+        this.url = url;
+        this.name = name;
+    }
+}
+
+public class VisorSettings
+{
+    public PlaceDescriptor[] previousPlaces;
+    public PlaceDescriptor[] PreviousPlaces {
+        get {
+            return previousPlaces;
+        }
+        private set {
+            previousPlaces = value;
+            Save();
+        }
+    }
+    public void addPlace(PlaceDescriptor place) {
+        List<PlaceDescriptor> places = new List<PlaceDescriptor>(this.PreviousPlaces);
+        if(places.Count > 10) {
+            places.RemoveAt(9);
+        }
+        places.Insert(0, place);
+        this.PreviousPlaces = places.ToArray();
+    }
+
+    public static VisorSettings GlobalSettings;
+    public static VisorSettings Load() {
+        string prefs = PlayerPrefs.GetString("VisorSettings");
+        if (prefs != null)
+        {
+            return LitJson.JsonMapper.ToObject<VisorSettings>(prefs);
+        } else {
+            return new VisorSettings();
+        }
+    }
+    public void Save() {
+        PlayerPrefs.SetString("VisorSettings", LitJson.JsonMapper.ToJson(this));
+    }
+}
+
 public class MenuScript : MonoBehaviour {
     public GameObject panel;
     public GameObject buttonPrefab;
@@ -20,22 +67,22 @@ public class MenuScript : MonoBehaviour {
 
 	void Start () {
         SetUrlCallback(Marshal.GetFunctionPointerForDelegate(new UrlCallback(this.UrlHandler)));
+        VisorSettings.GlobalSettings = VisorSettings.Load();
 
-        if(MenuParameters.lastError != null) {
+        if (MenuParameters.lastError != null) {
             GameObject text = (GameObject)Instantiate(errorPrefab);
             text.transform.SetParent(panel.transform);
             text.GetComponent<Text>().text = MenuParameters.lastError;
             MenuParameters.lastError = null;
         }
 
-        // todo: load previously opened alloplace URLs from history
-        for (int i = 0; i < 1; i++)
+        foreach(PlaceDescriptor place in VisorSettings.GlobalSettings.PreviousPlaces)
         {
             GameObject button = (GameObject)Instantiate(buttonPrefab);
             button.transform.SetParent(panel.transform);
             button.GetComponent<Button>().onClick.AddListener(ConnectTo);
-            button.name = "alloplace://localhost:21337";
-            button.transform.GetChild(0).GetComponent<Text>().text = button.name;
+            button.name = place.url;
+            button.transform.GetChild(0).GetComponent<Text>().text = place.name ?? place.url;
         }
     }
 
@@ -47,14 +94,16 @@ public class MenuScript : MonoBehaviour {
     private void UrlHandler(string url) {
         ConnectToUrl(url);
     }
-    private void ConnectToUrl(string url) {
-        MenuParameters.urlToOpen = url;
-        print("Opening url " + url);
-        SceneManager.LoadScene("Scenes/NetworkScene");
-    }
     public void ConnectTo() {
         string url = EventSystem.current.currentSelectedGameObject.name;
         ConnectToUrl(url);
+    }
+    private void ConnectToUrl(string url)
+    {
+        MenuParameters.urlToOpen = url;
+        print("Opening url " + url);
+        SceneManager.LoadScene("Scenes/NetworkScene");
+        VisorSettings.GlobalSettings.addPlace(new PlaceDescriptor(url, null));
     }
 
     [DllImport("AllovisorNativeExtensions")]
