6afd2090dd26682f7b9fb8c1e712a7e5a4ec8dbf
Nevyn Bengtsson <nevyn.jpg@gmail.com>
2019-03-17 17:25:43 +0100

    Script to download allonet on demand

--- Assets/Code/Editor/AlloVisorBuild.cs
+++ Assets/Code/Editor/AlloVisorBuild.cs
@@ -2,19 +2,77 @@
 using UnityEngine;
 using UnityEditor.Build.Reporting;
 using UnityEditor.iOS.Xcode;
+using System.Net;
+using GitHub.ICSharpCode.SharpZipLib.Zip;
+using System.IO;
 
-public class BuildPlayerExample : MonoBehaviour
+public class AllovisorBuilder : MonoBehaviour
 {
-    [MenuItem("Build/Upgrade allonet to latest master")]
+    [MenuItem("Build/Download allonet assets")]
+    public static void DownloadAllonet()
+    {
+
+        string currentVersion = File.Exists("Assets/allonet/allonet.cache") ?
+            File.ReadAllText("Assets/allonet/allonet.cache") : 
+            "-1";
+        string targetVersion = File.ReadAllText("Assets/allonet/allonet.lock");
+        if(currentVersion == targetVersion)
+        {
+            Debug.Log("Allonet is up to date");
+            return;
+        }
+        using (WebClient wc = new WebClient())
+        {
+            Download(wc, targetVersion, "Allonet-Linux-x64", "Allonet-Linux-x64/build/liballonet.so", "liballonet.so");
+            Download(wc, targetVersion, "Allonet-Windows-x64", "Allonet-Windows-x64/build/Debug/allonet.dll", "allonet.dll");
+            Download(wc, targetVersion, "Allonet-Mac-x64", "Allonet-Mac-x64/build/liballonet.dylib", "liballonet.bundle");
+        }
+        System.IO.File.WriteAllText("Assets/allonet/allonet.cache", targetVersion);
+        Debug.Log("Finished downloading Allonet for all platforms");
+    }
+
+    private static void Download(WebClient wc, string targetVersion, string artifactName, string path, string destination)
+    {
+        Debug.Log("Downloading Allonet "+artifactName);
+        var jsons = wc.DownloadString("https://dev.azure.com/alloverse/allonet/_apis/build/builds/" + targetVersion + "/artifacts?artifactName=" + artifactName + "&api-version=5.0");
+        var json = LitJson.JsonMapper.ToObject(jsons);
+        var artifactUrl = json["resource"]["downloadUrl"].ToString();
+        wc.DownloadFile(artifactUrl, "out.zip");
+        var zip = new ZipFile("out.zip");
+        ZipEntry entry = zip.GetEntry(path);
+        using (FileStream outputFile = File.Create("Assets/allonet/"+destination))
+        {
+            Stream zippedStream = zip.GetInputStream(entry);
+            byte[] dataBuffer = new byte[256 * 1024];
+            int readBytes;
+            while ((readBytes = zippedStream.Read(dataBuffer, 0, 256 * 1024)) > 0)
+            {
+                outputFile.Write(dataBuffer, 0, readBytes);
+                outputFile.Flush();
+            }
+        }
+        File.Delete("out.zip");
+    }
+
+    [MenuItem("Build/Upgrade allonet")]
     public static void UpgradeAllonet()
     {
-        Debug.Log("Not implemented yet");
+        using (WebClient wc = new WebClient())
+        {
+            var jsons = wc.DownloadString("https://dev.azure.com/alloverse/allonet/_apis/build/builds?api-version=5.0");
+            var json = LitJson.JsonMapper.ToObject(jsons);
+            var latestId = json["value"][0]["id"].ToString();
+            Debug.Log("Latest Allonet build ID is " + latestId);
+            System.IO.File.WriteAllText("Assets/allonet/allonet.lock", latestId);
+        }
+        DownloadAllonet();
     }
 
     // inspo: https://gist.github.com/sanukin39/997d8364d16c5c27dae75a3bc1f1f045
     [MenuItem("Build/[Mac] Build")]
     public static void MacBuild()
     {
+        DownloadAllonet();
         BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
         buildPlayerOptions.scenes = Scenes();
         buildPlayerOptions.locationPathName = "Build/Mac/Alloverse Visor";
@@ -80,6 +138,7 @@ public class BuildPlayerExample : MonoBehaviour
     [MenuItem("Build/[Android] Build and Install")]
     public static void AndroidBuild()
     {
+        DownloadAllonet();
         BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
         buildPlayerOptions.scenes = Scenes();
         buildPlayerOptions.locationPathName = "Build/Android/AlloverseVisor.apk";
