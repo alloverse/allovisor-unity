d01d708f5f6251b2a73844bc1853f896ddee2af7
Tobias Kask <tobias.kask@gmail.com>
2019-03-23 19:43:15 +0100

    Ability to rotate a sphere using controller input
    ...it's achieved using a custom script though, and only supports rotation. I was hoping I would be able to use something more out-of-the-box from the official VR bundle that would support rays, reticles, button presses and position tracking too.

---
+++ Assets/OculusVRTracking.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OculusVRTracking : MonoBehaviour
+{
+
+    public Transform controller;
+
+    public static bool leftHanded { get; private set; }
+
+    System.IO.StreamWriter recording;
+
+    void Awake()
+    {
+#if UNITY_EDITOR
+        leftHanded = false;        // (whichever you want to test here)
+#else
+        leftHanded = OVRInput.GetControllerPositionTracked(OVRInput.Controller.LTouch);
+#endif
+    }
+
+    void Update()
+    {
+        OVRInput.Controller c = leftHanded ? OVRInput.Controller.LTouch : OVRInput.Controller.RTouch;
+        if (OVRInput.GetControllerPositionTracked(c))
+        {
+            controller.localRotation = OVRInput.GetLocalControllerRotation(c);
+            controller.localPosition = OVRInput.GetLocalControllerPosition(c);
+        }
+    }
+}
